2a0bb8f5e72015fd9e8286910b5ee8365c5af168
Darren Zal <zaldarren@gmail.com>
2019-04-29 19:16:06 -0800

    Create EscrowERC721Factory.sol
    Factory for creating escrow contract for non fungible assets

---
+++ Contracts/Escrow/EscrowERC721Factory.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.00;
+import "./EscrowERC721.sol"; //fungible
+pragma experimental ABIEncoderV2;
+
+contract EscrowERC721Factory {
+    function addEscrowERC721(address[] memory _addresses, uint256 _tokenID, uint256 _price, uint256 _percentUpFront, bool _release, string memory _transType, string[] memory _feeTypes, uint256[] memory _fees) public payable returns (address){
+        EscrowERC721 newEscrowERC721 = (new EscrowERC721).value(msg.value)( _addresses, _tokenID, _price, _percentUpFront, _release, _transType, _feeTypes, _fees);
+        return address(newEscrowERC721);
+    }
+}
