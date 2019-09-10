0f7d27eea72a9065e9c79078ca9dc87917d5dbec
Darren Zal <zaldarren@gmail.com>
2019-09-09 22:18:20 -0800

    Update Materials.sol
    Add description.  Enable any ERC20, not just other FungibleTokens, to be transformable.

--- Contracts/Fungible/Materials.sol
+++ Contracts/Fungible/Materials.sol
@@ -1,9 +1,14 @@
-pragma solidity ^0.4.24;
+//This contract can create new fungible tokens (ERC20) with addNewMaterial()
+//Adding new materials is important to accomodate the different types of raw materials extracted around the world
+//This contract can transform any ERC20 into another ERC20 with transformMaterial()
+//transformation is important to oil and gas refining where raw material inputs are chemically transformed into new outputs
+
+pragma solidity ^0.5.00;
 import "../SafeMath.sol";
 import "./FungibleToken.sol";
 
 contract Materials{
-    //true => this address represents an existing material
+    //if true then this address represents an existing material
     mapping (address => bool) exists;
     //list of material contract addresses
     address[] public materialAddresses;
@@ -11,15 +16,16 @@ contract Materials{
     /**
     * @dev returns list of material addresses
     */
-    function getMaterials() public view returns (address[]){
+    function getMaterials() public view returns (address[] memory ){
         return materialAddresses;
     }
 
     /**
     * @dev allows adding a new material balance, allow duplicate name, symbol etc because address will be the unique ID
     */
-    function addNewMaterial(string _tokenName, uint8 _decimalUnits, string _tokenSymbol) public returns (address){
-            FungibleToken newTokenAddress = new FungibleToken(address(this), 0, _tokenName, _decimalUnits, _tokenSymbol);
+    function addNewMaterial(string memory _tokenName, uint8 _decimalUnits, string memory _tokenSymbol) public returns (address){
+            FungibleToken newToken = new FungibleToken(address(this), 0, _tokenName, _decimalUnits, _tokenSymbol);
+            address newTokenAddress = address(newToken);
             require(!exists[newTokenAddress]);
             exists[newTokenAddress] = true;
             materialAddresses.push(newTokenAddress);
@@ -28,26 +34,23 @@ contract Materials{
     }
 
     /**
-    * @dev allows deleting a material if there are no balances for that material
-    */
-    function removeMaterial(address _materialAddress, uint256 _materialIndex) public returns(bool) {
-        require(exists[_materialAddress] == true);
-        FungibleToken material = FungibleToken(_materialAddress);
-        require(material.totalSupply() == 0);
-        require(material.destruct(this) == true);
-        removeMaterialAddress(_materialIndex, _materialAddress);
-    }
-
-    /**
     * @dev allows transforming one material to another
     */
-    function transformMaterial(address _materialFrom, address _materialTo, uint256 _amountFrom, uint256 _amountTo, string _tokenName, uint8 _decimalUnits, string _tokenSymbol) public returns (bool){
-        require(exists[_materialFrom] == true);
-        FungibleToken material = FungibleToken(_materialFrom);
-        require(material.balanceOf(msg.sender) >= _amountFrom);
-        material.removeBalanceCustom(msg.sender, _amountFrom);
+    function transformMaterial(address _materialFrom, address _materialTo, uint256 _amountFrom, uint256 _amountTo, string memory _tokenName, uint8 _decimalUnits, string memory _tokenSymbol) public returns (bool){
         address addressMaterialTo = _materialTo;
         FungibleToken materialTo;
+        if(exists[_materialFrom]){ 
+            //The token being transformed exists as a Material, so we can decrease the supply using removeBalanceCustom
+            FungibleToken material = FungibleToken(_materialFrom);
+            require(material.balanceOf(msg.sender) >= _amountFrom);
+            material.removeBalanceCustom(msg.sender, _amountFrom);
+        } else { 
+            //The token being transformed must be a regular ERC20 which has approved this contract to transfer _amountFrom in order to lock it up
+            Token tokenFrom = Token(_materialFrom);
+            require(_amountFrom <= tokenFrom.balanceOf(msg.sender));
+            require(tokenFrom.allowance( msg.sender,  address(this)) >= _amountFrom);
+            tokenFrom.transferFrom(msg.sender, address(this), _amountFrom);
+        }
         if(addressMaterialTo == address(0)){
             addressMaterialTo = addNewMaterial(_tokenName, _decimalUnits, _tokenSymbol);
             materialTo = FungibleToken(addressMaterialTo);
@@ -57,20 +60,7 @@ contract Materials{
             materialTo = FungibleToken(_materialTo);
             materialTo.addBalanceCustom(msg.sender, _amountTo);
         }
-        TransformMaterial(_materialFrom, addressMaterialTo, _amountFrom, _amountTo);
-    }
-
-    /**
-    * @dev allows deleting an address for a given material if balance is 0
-    */
-    function removeMaterialAddress(uint256 _materialIndex, address _materalAddress) internal returns(bool) {
-        require(materialAddresses[_materialIndex] == _materalAddress);
-        for (uint i = _materialIndex; i<materialAddresses.length-1; i++){
-            materialAddresses[i] = materialAddresses[i+1];
-        }
-        delete materialAddresses[materialAddresses.length-1];
-        materialAddresses.length--;
-        return true;
+        emit TransformMaterial(_materialFrom, addressMaterialTo, _amountFrom, _amountTo);
     }
 
 
